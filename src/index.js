20dd15b4fc6f03544697bfc48f48ec3a1849f4a0
Tomas Okal <tomas.okal@gmail.com>
2023-01-10 17:23:27 -0600

    Provide initial set of files for debugging model load.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+import './style.css'
+
+import { StrictMode, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { createRoot } from 'react-dom/client'
+
+import Experience from './Experience.js'
+import Interface from './Interface.js'
+
+const root = createRoot(document.querySelector('#root'))
+
+root.render(
+    <StrictMode>
+        <Canvas
+            shadows
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 100,
+                position: [ 0, 15, 20 ]
+            } }>
+            <Suspense fallback={null}>
+                <Experience /> 
+            </Suspense>  
+        </Canvas>
+        <Interface />
+    </StrictMode>
+)
